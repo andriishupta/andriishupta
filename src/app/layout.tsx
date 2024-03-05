175a2417d16cd9c64d86622508a3982dd440e570
Andrii Shupta <andrii.shupta@gmail.com>
2024-03-05 21:08:46 +0200

    feat: add shadcn/ui
    Signed-off-by: Andrii Shupta <andrii.shupta@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,36 @@
-import {ReactNode} from "react";
+import {PropsWithChildren} from "react";
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import {ThemeProvider} from "@/components/theme-provider";
 import {cn} from "@/utils/cn";
 
-const inter = Inter({subsets: ["latin"]});
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Andrii Shupta - Full Stack Expert | andriishupta.dev",
   description: "With 8 years of experience in full-stack web development, I bring expertise in Next.js, React.js, Node.js, and NestJS. Specializing in building efficient and reliable web applications, I focus on SaaS, enterprise, MVPs, and internal projects. Leveraging technologies such as Supabase, TailwindCSS, and serverless architecture, I ensure fast and effective development from scratch, continuous feature delivery, and seamless third-party API integration. Based in Ukraine, I am ready to bring your web application ideas to life remotely.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
+export default function RootLayout({children}: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "container mx-auto")}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+    <body className={cn(
+      "min-h-screen bg-background font-sans antialiased",
+      fontSans.variable
+    )}>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import {Spotlight} from "@/components/ui/Spotlight";
-
 export default function Home() {
   return (
     <>
@@ -7,27 +5,11 @@ export default function Home() {
         Header
       </header>
       <main className="h-screen">
-        <div
-          className="h-full w-full flex md:items-center md:justify-center bg-white dark:bg-black/[0.96] antialiased relative overflow-hidden">
-          <Spotlight
-            className="-top-40 left-0 md:left-60 md:-top-20"
-            fill="currentcolor"
-          />
-          <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
-            <h1
-              className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-400 to-neutral-600 bg-opacity-100">
-              Spotlight <br/> is the new trend.
-            </h1>
-            <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-              Spotlight effect is a great way to draw attention to a specific part
-              of the page. Here, we are drawing the attention towards the text
-              section of the page. I don&apos;t know why but I&apos;m running out of
-              copy.
-            </p>
-          </div>
-        </div>
+        Main
       </main>
-      <footer></footer>
+      <footer>
+        Footer
+      </footer>
     </>
   );
 }
